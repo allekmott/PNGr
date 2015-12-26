b0ea2f74c9a1c270c826021d152fc97f8af3255a
tchieze <allekmott@gmail.com>
2015-12-26 12:41:30 -0700

    Initial commit

---
+++ pngr.c
@@ -0,0 +1,12 @@
+/* pngr.c
+ * Created: 26 Dec 2015
+ */
+
+#include <stdio.h>
+
+#define PNGR_VERSION "0.0.1"
+
+int main(int argc, char const *argv[]) {
+	printf("PNGr v%s\n", PNGR_VERSION);
+	return 0;
+}
